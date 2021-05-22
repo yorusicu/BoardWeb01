5e763a864fc81a525606c3212006adb96da5f30a
jimi <yorusicu@gmail.com>
2021-05-22 14:39:51 +0900

    applicationContext생성후 bean을 사용해 객체생성 및 종료전 처리를 담당하는 initMethod와 destroyMethod를 만들어 줌

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -2,6 +2,18 @@ package polymorphism;
 
 public class SamsungTV implements TV{
 	
+	public SamsungTV() {
+		System.out.println("====> SamsungTV 객체 생성");
+	}
+	
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리...");
+	}
+	
+	public void destoryMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직 처리...");
+	}
+	
 	public void powerOn() {
 		System.out.println("SamsungTv---전원을 켠다");
 	}
--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -1,15 +1,30 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV)factory.getBean(args[0]);
+		
+		// 1. Spring IoC 컨테이너를 구동한다.
+		AbstractApplicationContext factory =
+				new GenericXmlApplicationContext("applicationContext.xml");
+//		BeanFactory factory = new BeanFactory();
+		
+		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup)한다.
+		
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
 //		TV tv = new SamsungTV();
 //		TV tv = new LgTV();
+//		
+//		tv.powerOn();
+//		tv.powerOff();
+//		tv.VolumeUp();
+//		tv.VolumeDown();
 		
-		tv.powerOn();
-		tv.powerOff();
-		tv.VolumeUp();
-		tv.VolumeDown();
+		// 3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
 }

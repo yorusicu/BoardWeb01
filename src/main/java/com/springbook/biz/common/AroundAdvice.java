46b70c52fc6423c60ffefb17dcbf538f710b0477
jimi <200493019@naver.com>
2021-06-29 23:24:09 +0900

    [BoardWeb01][jimi]JoinPoint와 바인드 변수 이슈: Advice사용으로 비지니스 호출시 console출력 원인: log 및 exception출력으로 Debugging이 쉬워짐 수정: Advice, applicationContext

--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,13 +1,20 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
-	public Object aroundLog(ProceedingJoinPoint php) throws Throwable{
-		System.out.println("[BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용...");
-		Object returnObj = php.proceed();
-		System.out.println("[AFTER]: 비즈니스 메소드 수행 후에 처리행 내용...");
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
+String method = pjp.getSignature().getName();
 		
-		return returnObj;
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		Object obj = pjp.proceed();
+		
+		stopWatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+		
+		return obj;
 	}
 }
--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,7 +1,9 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class LogAdvice {
-	public void printLog() {
-		System.out.println("[공통 로그] LogAdvice_비즈니스 로직 수행 전 동작");
+	public void printLog(JoinPoint jp) {
+			System.out.println("[공통 로그] LogAdvice_비즈니스 로직 수행 전 동작");
 	}
 }
